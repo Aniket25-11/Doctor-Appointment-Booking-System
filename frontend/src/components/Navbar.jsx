37d4e0840352d8a89bcc44d703ac3bd637e4a234
Aniket25-11 <Www.aniketbhojapure@gmail.com>
2025-01-27 20:31:24 +0530

    My Appointment Page Done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AppContext } from "../context/AppContext";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const { token, setToken,userData } = useContext(AppContext);
+  const { token, setToken, userData } = useContext(AppContext);
   const [showMenu, setShowMenu] = useState(false);
   const logout = () => {
     setToken(false);
@@ -20,7 +20,6 @@ const Navbar = () => {
         src={assets.logo}
         alt="Logo"
       />
-      {/* <img className='w-40 h-12 object-cover cursor-pointer rounded-xl' src={assets.aptifyLogo} alt="Logo" /> */}
 
       <ul className="hidden md:flex items-start gap-5 font-medium">
         <NavLink to="/">
@@ -45,7 +44,7 @@ const Navbar = () => {
           <div className="flex items-center gap-2 cursor-pointer group relative">
             <img className="w-12 h-12 rounded-full object-full" src={userData.image} alt="" />
             <img className="w-2.5" src={assets.dropdown_icon} alt="" />
-            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block ">
+            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
               <div className="min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4">
                 <p
                   onClick={() => navigate("my-profile")}
@@ -72,6 +71,22 @@ const Navbar = () => {
           >
             Create Account
           </button>
+        )}
+        <img onClick={() => setShowMenu(true)} className="w-6 md:hidden" src={assets.menu_icon} alt="" />
+        {/* ------ Mobile Menu ------ */}
+        {showMenu && (
+          <div className={` ${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+            <div className="flex items-center justify-between px-5 py-6">
+              <img className="w-36" src={assets.logo} alt="" />
+              <img className="w-7" onClick={() => setShowMenu(false)} src={assets.cross_icon} alt="" />
+            </div>
+            <ul className="flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium">
+              <NavLink onClick={()=>setShowMenu(false)} to="/"><p className='px-4 py-2 rounded inline-block'>Home</p></NavLink>
+              <NavLink onClick={()=>setShowMenu(false)} to="/doctors"><p className='px-4 py-2 rounded inline-block'>ALL DOCTORS</p></NavLink>
+              <NavLink onClick={()=>setShowMenu(false)} to="/about"><p className='px-4 py-2 rounded inline-block'>ABOUT</p></NavLink>
+              <NavLink onClick={()=>setShowMenu(false)} to="/contact"><p className='px-4 py-2 rounded inline-block'>CONTACT</p></NavLink>
+            </ul>
+          </div>
         )}
       </div>
     </div>
--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -3,10 +3,11 @@ import { useNavigate, useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 
 const Doctors = () => {
-  const { speciality } = useParams();
-  // console.log(speciality);
 
+  const { speciality } = useParams();
   const [filterDoc, setFilterDoc] = useState([]);
+  const [showFilter, setShowFilter] = useState(false)
+
   const navigate = useNavigate();
   const { doctors } = useContext(AppContext);
 
@@ -26,6 +27,7 @@ const Doctors = () => {
     <div>
       <p className="text-gray-600">Browse through the doctors specialist.</p>
       <div className="flex flex-col sm:flex-row items-start gap-5 mt-5">
+        <button className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`} onClick={()=>setShowFilter(prev => !prev)}>Filters</button>
         <div className="flex flex-col gap-4 text-sm text-gray-600">
           <p
             onClick={() =>
